a436eb3ff48717922e9de81a37404dc85dc8c099
ValentinBulnes <142333048+ValentinBulnes@users.noreply.github.com>
2023-10-10 16:49:08 -0300

    Update articulos_db.sql

--- articulos_db.sql
+++ articulos_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2023 at 05:12 AM
+-- Generation Time: Oct 10, 2023 at 09:36 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -32,6 +32,15 @@ CREATE TABLE `categoria` (
   `Nombre` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `categoria`
+--
+
+INSERT INTO `categoria` (`Categoria_id`, `Nombre`) VALUES
+(1, 'monitores'),
+(2, 'procesadores'),
+(3, 'perifericos');
+
 -- --------------------------------------------------------
 
 --
@@ -44,6 +53,43 @@ CREATE TABLE `producto` (
   `Precio` int(11) NOT NULL,
   `Categoria_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `producto`
+--
+
+INSERT INTO `producto` (`Producto_id`, `Nombre`, `Precio`, `Categoria_id`) VALUES
+(1, 'Sony 8k', 210, 1),
+(2, 'Philips 4k 144hz', 180, 1),
+(3, 'Sony 8k 360hz', 370, 1),
+(4, 'Samsung 4k', 350, 1),
+(5, 'AMD Ryzen 7 ', 180, 2),
+(6, 'AMD Ryzen 4', 200, 2),
+(7, 'AMD Ryzen 2', 150, 2),
+(8, 'AMD Ryzen 1', 80, 2),
+(9, 'Teclado Samsung', 180, 3),
+(10, 'Teclado SONY', 180, 3),
+(11, 'Teclado Gamer', 250, 3),
+(12, 'Teclado Mecanico', 200, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(255) NOT NULL,
+  `contraseña` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `contraseña`) VALUES
+(1, 'webadmin', '$2a$12$vcYDk0DzerX3HltbBoSY2OOgke2ygFNOtk0gCIT8vommp/V78KSRy');
 
 --
 -- Indexes for dumped tables
@@ -61,6 +107,28 @@ ALTER TABLE `categoria`
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`Producto_id`),
   ADD KEY `Categoria_id` (`Categoria_id`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `Producto_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
